e8748600a8c9c355be5ada6a0dc86dc777df6697
AltugAltintas <altuga@gmail.com>
2021-04-05 12:46:49 +0300

    Healt check for initial-post-exists

--- blogpad/src/main/java/airhacks/blogpad/posts/control/Initialzer.java
+++ blogpad/src/main/java/airhacks/blogpad/posts/control/Initialzer.java
@@ -2,26 +2,56 @@ package airhacks.blogpad.posts.control;
 
 
 import airhacks.blogpad.posts.entity.Post;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 
 @Startup
 @Singleton
 public class Initialzer {
 
+    public static final String TITLE = "initial";
+
     @Inject
     PostStore postStore;
 
     @PostConstruct
     public void installFirstPost() {
-        Post post =     this.createInitialPost();
+        if(postExists()) {
+            return;
+        }
+        Post post =    this.createInitialPost();
         this.postStore.createNew(post);
     }
 
     Post createInitialPost() {
-        return new  Post("initial" , "welcome to blogpad");
+        return new  Post(TITLE, "welcome to blogpad");
     }
+
+
+    boolean postExists() {
+        var post = this.fetchPost();
+        if (post == null) {
+            return false;
+        }
+        return TITLE.equalsIgnoreCase(post.title);
+    }
+
+    Post fetchPost() {
+        return this.postStore.read(TITLE);
+    }
+
+    @Produces
+    @Liveness
+    public HealthCheck initialExisits() {
+        return () -> HealthCheckResponse.named("initial-post-exists").state(this.postExists()).build();
+    }
+
+
 }
